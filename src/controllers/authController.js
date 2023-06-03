d39d807beffff43449fa992184f0dbc3d8a50c6d
higorbeleza <higorbeleza49@gmail.com>
2023-06-03 17:18:43 -0400

    feat(higorbeleza): user signup add

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,8 @@
+const mongoose = require('mongoose');
 const { validationResult, matchedData } = require('express-validator');
+
+const User = require('../models/User');
+const State = require('../models/State');
 
 module.exports = {
     signin: async (req, res) => {
@@ -10,6 +14,34 @@ module.exports = {
             res.json({error: errors.mapped()});
             return;
         };
+
+        const data = matchedData(req);
+
+        const user = await User.findOne({
+            email: data.email
+        });
+        if(user) {
+            res.json({
+                error: {email: {msg: 'E-mail já existe!'}}
+            });
+            return;
+        }
+
+        if(mongoose.Types.ObjectId.isValid(data.state)) {
+            const stateItem = await State.findById(data.state);
+            if(!stateItem) {
+                res.json({
+                    error: {state: {msg: 'Estado não existe!'}}
+                });
+                return;
+            }
+        }else {
+            res.json({
+                error: {state: {msg: 'Código de estado inválido!'}}
+            });
+            return;
+        }
+
         res.json({allRight: true});
     },
 };
