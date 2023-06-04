8ae4d67ff430f478e48b457d0debe941081079c5
higorbeleza <higorbeleza49@gmail.com>
2023-06-04 19:32:08 -0400

    feat(higorbeleza): get user info

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,7 @@
 const State = require('../models/State');
+const User = require('../models/User');
+const Ads = require('../models/Ad');
+const Category = require('../models/Category');
 
 module.exports = {
   getStates: async (req, res) => {
@@ -6,7 +9,37 @@ module.exports = {
     res.json({states});
   },
   info: async (req, res) => {
+    let token = req.query.token;
 
+    const user = await User.findOne({token});
+    const state = await State.findById(user.state);
+    const ads = await Ads.find({idUser: user._id.toString()});
+
+    let adList = [];
+    for(let i in ads){
+
+      const cat = await Category.findById(ads[i].category);
+
+      adList.push({
+        id: ads[i]._id,
+        status: ads[i].status,
+        images: ads[i].images,
+        dateCreated: ads[i].dateCreated,
+        title: ads[i].title,
+        price: ads[i].price,
+        priceNegotiable: ads[i].ads[i].priceNegotiable,
+        description: ads[i].description,
+        views: ads[i].views,
+        category: cat.slug
+      });
+    }
+
+    res.json({
+      name: user.name,
+      email: user.email,
+      state: state.name,
+      ads: adList
+    });
   },
   editAction: async (req, res) => {
 
