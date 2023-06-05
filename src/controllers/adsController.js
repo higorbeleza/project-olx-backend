6cf657c89758f7a1081b5716fb7515eac860de47
higorbeleza <higorbeleza49@gmail.com>
2023-06-05 13:32:56 -0400

    feat(higorbeleza): get categories

--- src/controllers/adsController.js
+++ src/controllers/adsController.js
@@ -1,6 +1,19 @@
+const Category = require('../models/Category');
+
 module.exports = {
     getCategories: async (req, res) => {
+        const cats = await Category.find();
 
+        let categories = [];
+
+        for(let i in cats){
+            categories.push({
+                ...cats[i]._doc,
+                img: `${process.env.BASE}/assets/imges/${cats[i].slug}.png`
+            });
+        }
+
+        res.json({categories});
     },
     addAction: async (req, res) => {
 
