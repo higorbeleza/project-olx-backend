644fb70f21ad2332fe2d1d396813f45ee01f6ced
higorbeleza <higorbeleza49@gmail.com>
2023-06-05 14:12:27 -0400

    feat(higorbeleza): add new ad

--- src/controllers/adsController.js
+++ src/controllers/adsController.js
@@ -1,4 +1,6 @@
 const Category = require('../models/Category');
+const User = require('../models/User');
+const Ad = require('../models/Ad');
 
 module.exports = {
     getCategories: async (req, res) => {
@@ -16,7 +18,32 @@ module.exports = {
         res.json({categories});
     },
     addAction: async (req, res) => {
+        let {title, price, priceneg, desc, cat, token} = req.body;
+        const user = await User.findOne({token}).exec();
 
+        if(!title || !cat) {
+            res.json({error: 'Título e/ou categoria não foram preenchidos'});
+            return;
+        }
+
+        if(price) {
+            price = price.replace('.', '').replace(',', '.').replace('R$ ', '');
+            price = parseFloat(price);
+        }else {
+            price = 0;
+        }
+
+        const newAd = new Ad();
+        newAd.status = true;
+        newAd.idUser = user._id;
+        newAd.state = user.state;
+        newAd.dateCreated = new Date();
+        newAd.title = title;
+        newAd.category = cat;
+        newAd.price = price;
+        newAd.priceNegotiable = (priceneg=='true') ? true:false;
+        newAd.description = desc;
+        newAd.views = 0;
     },
     getList: async (req, res) => {
 
