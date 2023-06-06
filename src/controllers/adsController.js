aafcd861964598ab3f5bf681b769fa1d19f67aac
higorbeleza <higorbeleza49@gmail.com>
2023-06-06 10:38:07 -0400

    feat(higorbeleza): ads updates

--- src/controllers/adsController.js
+++ src/controllers/adsController.js
@@ -214,6 +214,61 @@ module.exports = {
         });
     },
     editAction: async (req, res) => {
+        let {id} = req.params;
+        let {title, status, priceneg, desc, cat, images, token} = req.body;
+
+        if(id.lenght < 12) {
+            res.json({error: 'ID inválido'});
+            return;
+        }
+
+        const ad = await Ad.findById(id).exec();
         
+        if(!ad) {
+            res.json({error: 'Anúncio inexistente'});
+            return;
+        }
+
+        const user = await User.findOne({token}).exec();
+        if(user._id.toString() !== ad.idUser) {
+            res.json({error: 'Este anúncio não é seu'});
+            return;
+        }
+
+        let updates = {};
+
+        if(title) {
+            updates.title = title;
+        }
+        if(price) {
+            price = price.replace('.', '').replace(',', '.').replace('R$ ', '');
+            price = parseFloat(price);
+            updates.price = price;
+        }
+        if(priceneg) {
+            updates.priceNegotiable = priceneg;
+        }
+        if(status) {
+            updates.status = status;
+        }
+        if(desc) {
+            updates.description = desc;
+        }
+        if(cat) {
+            const category = await Category.findOne({slug: cat}).exec();
+            if(!cat) {
+                res.json({error: 'Categoria inexistente'});
+                return;
+            }
+            updates.category = category._id.toString();
+        }
+
+        if(images) {
+            updates.images = images;
+        }
+
+        await Ad.findByIdAndUpdate(id, {$set: updates});
+
+        res.json({error: ''});
     }
 };
